472dc2eb487b70d347e746c34975bef2c464244d
abdelrahman adel <38409101+abdelrahmanadel30@users.noreply.github.com>
2018-04-16 02:13:44 +0200

    tanatos

---
+++ js/tanatos.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    
+$(".down i").click(function(){
+
+ $("body").animate({
+
+  scrollTop: $(".work").offset().top
+
+  }, 1000);
+  });
+  
+    $(".design h1").fadeIn(1000);
+
+
+    
+});
